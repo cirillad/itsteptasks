f8a6b71f1472e1175b3fb836a9ed0f27046fdf7a
cirillad <burba.2018london@gmail.com>
2023-12-07 17:00:14 +0200

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,29 @@
+# task1
+
+# first_num = int(input('Enter the first number : '))
+# second_num = int(input('Enter the second number : '))
+# third_num = int(input('Enter the third number : '))
+#
+# result1 = first_num + second_num + third_num
+# result2 = first_num * second_num * third_num
+#
+# print(f'Addition result : {result1}')
+# print(f'Multiplication result : {result2}')
+
+
+# task2
+
+# first_num = int(input('Введіть суму вашої зарплатні : '))
+# second_num = int(input('Введіть витрати місячного платежу за кредитом у банку : '))
+# third_num = int(input('Введіть витрати на заборгованість за комунальні послуги : '))
+#
+# result = first_num - (second_num + third_num)
+# print(f'Залишок зарплатні : {result}')
+
+
+# task3
+first_num = int(input('Enter the length of first rhombus diagonal  : '))
+second_num = int(input('Enter the length of second rhombus diagonal  : '))
+
+result = first_num * second_num / 2
+print(f'Rhombus area : {result}')
